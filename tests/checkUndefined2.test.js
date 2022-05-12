5005609ceae109cb30a6e11bec7bbd757cbbd98b
souvik666 <peacedynasty0@gmail.com>
2022-05-12 13:10:36 +0530

    checkUndefined2 factorialof import issue fixed

--- tests/checkUndefined2.test.js
+++ tests/checkUndefined2.test.js
@@ -1,6 +1,6 @@
 import { Compile } from "../Compiler/main";
-import { undefined2 } from "../testCodes/checkundefined2";
-
+/* import { undefined2 } from "../testCodes/checkundefined2"; */
+import { undefined2 } from "../testCodes/checkUndefined2";
 test("Checks Undefined error", () => {
   const expected = ["Cannot set c to undefined : b is undefined "];
   var { linebylineOutput } = Compile(undefined2);
--- tests/factorialof.test.js
+++ tests/factorialof.test.js
@@ -1,6 +1,6 @@
 import { Compile } from "../Compiler/main";
-import { factorialOf } from "../testCodes/factorialOf";
-
+/* import { factorialOf } from "../testCodes/factorialOf"; */
+import { factorialOf } from "../testCodes/factorialof";
 test("Checks factorial", () => {
   const expected = ["Factorial of  5  is  120"];
   var { linebylineOutput } = Compile(factorialOf);
